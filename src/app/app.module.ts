825249d0af3f074b99d7d6fdb7eda395512591e9
gabo-x86 <201706699@est.umss.edu>
2021-04-23 21:14:16 -0400

    Creacion componente detalle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule  ({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ListComponent
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
