8960694f01bf6871d2b926a69ee6c2d535be3728
Ricardo <ric75711904@gmail.com>
2021-04-25 03:31:14 -0400

    busqueda inconclusa, busqueda para alimento no encontrado concluida

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2'
 //Servicio
 import { ProductService } from '../services/product.service';
 
@@ -16,6 +16,7 @@ export class ListComponent implements OnInit {
   productList:Product[];
   hoverList=[false, false, false, false, false];
   prevHover:number;
+  buscar: string;//ri
 
   constructor(private productService:ProductService) { }
 
@@ -36,6 +37,42 @@ export class ListComponent implements OnInit {
       });
     });
   }
+  consulProductos(){
+    this.productService.getProducts()
+    .snapshotChanges()//Escucha la BD
+    .subscribe(item=>{
+      this.productList=[];
+      item.forEach(element=>{
+        let x = element.payload.toJSON();//Convertir a JSON
+        x["$key"]=element.key;
+        this.productList.push(x as Product);
+      });
+      const Swal = require('sweetalert2')
+  this.productList=this.productList.filter(data=>{
+    return data.$key.toString().trim()==this.buscar;
+  })
+  if(this.productList.length==0){
+    this.getValues();
+    Swal.fire({
+      position: 'top-end',
+      type: 'success',
+      title: 'No se ha encontrado ese alimento',
+      showConfirmButton:false,
+      timer: 1500
+  
+    })
+  }else{
+    
+    Swal.fire({
+      position: 'top-end',
+      type:'success',
+      title: 'Alimento encontrado!',
+      showConfirmButton: false,
+      timer: 1500
+
+    })
+    }
+    })};
 
   changeHover(index){
     this.hoverList[this.prevHover]=!this.hoverList[this.prevHover];
@@ -43,7 +80,7 @@ export class ListComponent implements OnInit {
     this.prevHover = index;
   }
 
-  categorySearch(category){
+ categorySearch(category){
     this.productService.getProducts()
     .snapshotChanges()//Escucha la BD
     .subscribe(item=>{
@@ -57,6 +94,5 @@ export class ListComponent implements OnInit {
       });
     });
   }
-
 
 }
