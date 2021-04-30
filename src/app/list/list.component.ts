6bd7ab11d7d1c1fc4b4d0506747805a32d692b5f
gabo-x86 <201706699@est.umss.edu>
2021-04-29 21:23:00 -0400

    bug-fix Busqueda por coincidencia

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -5,6 +5,7 @@ import { ProductService } from '../services/product.service';
 
 //Modelo de datos
 import { Product } from '../models/product';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-list',
@@ -43,16 +44,19 @@ export class ListComponent implements OnInit {
   consulProductos(searchValue){
     let itsFind:Boolean;
     itsFind=false;
+    let stringNorm:string;
+
     this.productService.getProducts()
     .snapshotChanges()//Escucha la BD
     .subscribe(item=>{
       this.productList=[];
       item.forEach(element=>{
         let x = element.payload.toJSON();//Convertir a JSON
-        x["$key"]=element.key;        
-        if(x["name"]==searchValue.value.toString().toUpperCase()){
+        x["$key"]=element.key;
+        stringNorm = this.getCleanedString(searchValue.value);//Normalizar cadena
+        if(x["name"].startsWith(stringNorm.toUpperCase())){
           this.productList.push(x as Product);
-          itsFind=true;          
+          itsFind=true;
         }
       });
 
@@ -81,6 +85,29 @@ export class ListComponent implements OnInit {
       this.changeHover(-1);
 
     })};
+
+
+  getCleanedString(cadena){
+      var specialChars = "!@#$^&%*()+=-[]\/{}|:<>?,.";
+   
+      for (var i = 0; i < specialChars.length; i++) {
+          cadena= cadena.replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+      }   
+   
+      cadena = cadena.toLowerCase();
+   
+      //cadena = cadena.replace(/ /g,"_");
+   
+      cadena = cadena.replace(/á/gi,"a");
+      cadena = cadena.replace(/é/gi,"e");
+      cadena = cadena.replace(/í/gi,"i");
+      cadena = cadena.replace(/ó/gi,"o");
+      cadena = cadena.replace(/ú/gi,"u");
+      //cadena = cadena.replace(/ñ/gi,"n");
+      return cadena;
+  }
+
+
 
   changeHover(index){
     this.hoverList[this.prevHover]=!this.hoverList[this.prevHover];
@@ -115,8 +142,8 @@ export class ListComponent implements OnInit {
             event.charCode==243 || event.charCode==218 || event.charCode==250)
   }
 
-  searchSuccess(){
-    if(this.success){
+  listLength(){
+    if(this.productList.length<=1){
       return "80vh"
     }else return "max-content"
   }
