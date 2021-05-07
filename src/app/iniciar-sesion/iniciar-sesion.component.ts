4dc43cce94e3e6117dd9804e746975e5b1cbea95
gabo-x86 <201706699@est.umss.edu>
2021-05-07 11:46:15 -0400

    Pop-ups y redireccion a lista de alimentos en login exitoso

--- src/app/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,29 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup,Validators, FormControl} from '@angular/forms';
 import {AngularFireAuth} from '@angular/fire/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
   styleUrls: ['./iniciar-sesion.component.css']
 })
 export class IniciarSesionComponent implements OnInit {
+
   loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
   });
-  constructor(public auth: AngularFireAuth) { }
 
-  ngOnInit(): void {
-    
-  }
+  constructor(public auth: AngularFireAuth, private router: Router) { }
+
+  ngOnInit(): void {  }
+
   async onLogin(){
     const { email, password } = this.loginForm.value;
+    const Swal = require('sweetalert2');
     this.auth.auth.signInWithEmailAndPassword(email, password)
     .then(res=>{
-      console.log("INICIO DE SESION EXITOSO!!!");
+      Swal.fire({
+        position: 'top-center',
+        type: 'success',
+        title: 'Bienvenido'+email,
+        showConfirmButton:false,
+        timer: 2000
+      })
+      this.router.navigate(['/']);
     })
-    .catch(err=>console.log("Error: ",err));
+    .catch(err=>{
+      Swal.fire({
+        position: 'top-center',
+        type: 'success',
+        title: 'Datos Incorrectos',
+        showConfirmButton:false,
+        timer: 2000      
+      })
+      console.log("Error: ",err)
+    });
   }
+  
+        
+
+
 
   async onLogOut(){
     this.auth.auth.signOut();
