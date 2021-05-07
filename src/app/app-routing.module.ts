d822b6f6af9daf4ff9fcfa5d022b2649dac09d4c
gabo-x86 <201706699@est.umss.edu>
2021-05-07 11:14:58 -0400

    Creacion de ruta de lista a login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 
 const routes: Routes = [
   {path: '', component: ListComponent},
-  {path: 'detail', component: DetailComponent}
+  {path: 'detail', component: DetailComponent},
+  {path: 'login', component: IniciarSesionComponent}
   
 ];
 
