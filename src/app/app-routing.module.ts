167f3d1184cf2ddd1c97a7a57d39964e808abab5
gabo-x86 <201706699@est.umss.edu>
2021-05-10 20:34:04 -0400

    Ruta de lista a registro de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { RegistroComponent } from './registro/registro.component';
 
 
 const routes: Routes = [
   {path: '', component: ListComponent},
   {path: 'detail', component: DetailComponent},
-  {path: 'login', component: IniciarSesionComponent}
-  
+  {path: 'login', component: IniciarSesionComponent},
+  {path: 'signup', component: RegistroComponent}  
 ];
 
 @NgModule({
