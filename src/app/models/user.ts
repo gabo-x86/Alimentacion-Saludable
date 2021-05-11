14aa612b2045aa52646e0b9c01688f362835a1ef
gabo-x86 <201706699@est.umss.edu>
2021-05-11 17:29:08 -0400

    Creación de modelo y servicio para almacenar datos de usuario

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    $key: string;
+    alias: string;
+    pass: string;
+    email: string;
+    name: string;
+    lastName: string;
+    bornDate: Date;
+    gender: string;
+    weight: string;
+    height: string;
+}
