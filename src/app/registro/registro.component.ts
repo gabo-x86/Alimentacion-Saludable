64b83831871586d2ad3a06eea6e639340cefaa3d
gabo-x86 <201706699@est.umss.edu>
2021-05-12 20:35:09 -0400

    Regsitro de usuarios en RealTime DB

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import {FormBuilder, FormGroup,Validators, FormControl} from '@angular/forms';
 import {AngularFireAuth} from '@angular/fire/auth';
-import { Router } from '@angular/router';
 
+import { NgForm } from '@angular/forms';
+import { UserService } from './../services/user.service';
+import { User } from './../models/user';
 
 @Component({
   selector: 'app-registro',
@@ -11,10 +13,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers();
+    this.resetForm();
+  }
+
+  onSubmit(userForm: NgForm){
+    if(userForm.value.$key == null)
+      this.userService.insertUser(userForm.value)
+    else      
+      //this.userService.UpdateProduct(userForm.value);
+      //this.resetForm(userForm);
+      console.log("erroooooor!!");
+  }
+
+  resetForm(userForm?: NgForm){
+    if(userForm != null){
+      userForm.reset();
+      this.userService.selectedUser = new User();
+    }
   }
 
 }
