68fd70c28dff50577a6b703371e61d7820378da0
gabo-x86 <201706699@est.umss.edu>
2021-05-19 23:25:03 -0400

    bug-fix No se debe volver al login o registro cuando ya se inició sesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 import { RegistroComponent } from './registro/registro.component';
+import { LoginGuardGuard } from './guards/login-guard.guard';
 
 
 const routes: Routes = [
   {path: '', component: ListComponent},
   {path: 'detail', component: DetailComponent},
-  {path: 'login', component: IniciarSesionComponent},
-
-  {path: 'registro', component: RegistroComponent}
+  {path: 'login', component: IniciarSesionComponent, canActivate:[LoginGuardGuard]},
+  {path: 'registro', component: RegistroComponent, canActivate:[LoginGuardGuard]}
   
 ];
 
---
+++ src/app/guards/login-guard.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardGuard implements CanActivate {
+  constructor(private router:Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(localStorage.getItem('user')=="undefined" || localStorage.getItem('user')==null){
+      return true;
+    } return false;
+  }
+  
+}
