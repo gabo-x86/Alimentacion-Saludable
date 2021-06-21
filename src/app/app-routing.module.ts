4f238ba019ea7bd5a5f121fc080b454d17e2cd8a
jhasmany <201802037@est.umss.edu>
2021-06-20 22:47:11 -0400

    interfaz 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
@@ -9,13 +9,15 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { RecommendProductComponent } from './recommend-product/recommend-product.component';
 
 
+
 const routes: Routes = [
   {path: '', component: ListComponent},
   {path: 'detail', component: DetailComponent},
   {path: 'login', component: IniciarSesionComponent, canActivate:[LoginGuardGuard]},
   {path: 'registro', component: RegistroComponent, canActivate:[LoginGuardGuard]},
   {path: 'add-product', component: AddProductComponent},
-  {path: 'recommend-product', component: RecommendProductComponent}
+  {path: 'recommend-product', component: RecommendProductComponent},
+  
 ];
 
 @NgModule({
